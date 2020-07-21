0fad66ff91d4d07acae8647e07ce1048b570dc39
Igor <ig.rudenko1@yandex.ru>
2020-07-22 00:44:27 +0300

    for windows (.exe)

--- music_sorted.py
+++ music_sorted.py
@@ -11,7 +11,7 @@ for track in music_list:
     print(track)
     # FLAC
     if str(findall(r".+\.(\S+)", track)) == "['flac']":
-        tags_flac = mutagen.flac.FLAC(f"music\\{track}")        # Вытягиваем теги
+        tags_flac = mutagen.flac.FLAC(f"{path}\\{track}")        # Вытягиваем теги
         tags_flac_str = str(tags_flac.pprint()).split('\n')     # Преобразуем в список
 
         album_flac = str([line.split("=")[1] for line in tags_flac_str if "ALBUM=" in line])[2:-2]  # Переменная "Альбом"
@@ -26,7 +26,7 @@ for track in music_list:
 
     # MP3
     if str(findall(r".+\.(\S+)", track)) == "['mp3']":
-        tag_mp3 = mutagen.File(f"music\\{track}")         # Вытягиваем теги
+        tag_mp3 = mutagen.File(f"{path}\\{track}")         # Вытягиваем теги
         tags_mp3 = str(tag_mp3.pprint()).split('\n')      # Преобразуем в список
 
         album_mp3 = str([line.split("=")[1] for line in tags_mp3 if "TALB=" in line])[2:-2]     # Переменная "Альбом"
