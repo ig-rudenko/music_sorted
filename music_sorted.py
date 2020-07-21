1d2da0751249313c27231784aec4261eaac2654d
Igor <ig.rudenko1@yandex.ru>
2020-07-22 01:14:54 +0300

    for windows (.exe)

--- music_sorted.py
+++ music_sorted.py
@@ -14,10 +14,14 @@ for track in music_list:
         if str(findall(r".+\.(\S+)", track)) == "['flac']":
             tags_flac = mutagen.flac.FLAC(f"{path}\{track}")        # Вытягиваем теги
             tags_flac_str = str(tags_flac.pprint()).split('\n')     # Преобразуем в список
-
-            album_flac = str([line.split("=")[1] for line in tags_flac_str if "ALBUM=" in line])[2:-2]  # Переменная "Альбом"
-            year_flac = str([line.split("=")[1] for line in tags_flac_str if "DATE=" in line])[2:-2]  # Переменная "Год (Альбома)"
-            artist_flac = str([line.split("=")[1] for line in tags_flac_str if "ALBUMARTIST=" in line])[2:-2]  # Переменная "Артист"
+            print(tags_flac_str)
+            album_flac = str([line.split("=")[1] for line in tags_flac_str if "album=" in line.lower()])[2:-2]  # Переменная "Альбом"
+            album_flac = album_flac.replace(":", " ").replace("\\", " ").replace("/", " ").replace("*", " ").replace("?", " ").replace("\"", " ").replace("<", " ").replace(">", " ").replace("|", " ")
+            year_flac = str([line.split("=")[1] for line in tags_flac_str if "date=" in line.lower()])[2:-2]  # Переменная "Год (Альбома)"
+            year_flac = year_flac.replace(":", " ").replace("\\", " ").replace("/", " ").replace("*", " ").replace("?", " ").replace("\"", " ").replace("<", " ").replace(">", " ").replace("|", " ")
+            artist_flac = str([line.split("=")[1] for line in tags_flac_str if "albumartist=" in line.lower()])[2:-2]  # Переменная "Артист"
+            artist_flac = artist_flac.replace(":", " ").replace("\\", " ").replace("/", " ").replace("*", " ").replace("?", " ").replace("\"", " ").replace("<", " ").replace(">", " ").replace("|", " ")
+            print(artist_flac, year_flac, album_flac)
 
             if not os.path.exists(f"{path}\{artist_flac}\[{year_flac}] {album_flac}"): # Если папка с альбомом НЕ создана, то...
                 os.makedirs(f"{path}\{artist_flac}\[{year_flac}] {album_flac}")   # ...создаем
@@ -30,9 +34,12 @@ for track in music_list:
             tag_mp3 = mutagen.File(f"{path}\{track}")         # Вытягиваем теги
             tags_mp3 = str(tag_mp3.pprint()).split('\n')      # Преобразуем в список
 
-            album_mp3 = str([line.split("=")[1] for line in tags_mp3 if "TALB=" in line])[2:-2]     # Переменная "Альбом"
-            year_mp3 = str([line.split("=")[1] for line in tags_mp3 if "TDRC=" in line])[2:-2]      # Переменная "Год (Альбома)"
-            artist_mp3 = str([line.split("=")[1] for line in tags_mp3 if "TPE1=" in line])[2:-2]    # Переменная "Артист"
+            album_mp3 = str([line.split("=")[1] for line in tags_mp3 if "TALB=" in line.upper()])[2:-2]     # Переменная "Альбом"
+            album_mp3 = album_mp3.replace(":", " ").replace("\\", " ").replace("/", " ").replace("*", " ").replace("?", " ").replace("\"", " ").replace("<", " ").replace(">", " ").replace("|", " ")
+            year_mp3 = str([line.split("=")[1] for line in tags_mp3 if "TDRC=" in line.upper()])[2:-2]      # Переменная "Год (Альбома)"
+            year_mp3 = year_mp3.replace(":", " ").replace("\\", " ").replace("/", " ").replace("*", " ").replace("?", " ").replace("\"", " ").replace("<", " ").replace(">", " ").replace("|", " ")
+            artist_mp3 = str([line.split("=")[1] for line in tags_mp3 if "TPE1=" in line.upper()])[2:-2]    # Переменная "Артист"
+            artist_mp3 = artist_mp3.replace(":", " ").replace("\\", " ").replace("/", " ").replace("*", " ").replace("?", " ").replace("\"", " ").replace("<", " ").replace(">", " ").replace("|", " ")
 
             if not os.path.exists(f"{path}\{artist_mp3}\[{year_mp3}] {album_mp3}"): # Если папка с альбомом НЕ создана, то...
                 os.makedirs(f"{path}\{artist_mp3}\[{year_mp3}] {album_mp3}")   # ...создаем
